21ecf77f3f0fad429ad84cdcf99be17892bbdc0f
DanielMCondor <dmantillacondor@gmail.com>
2024-03-26 18:32:33 -0500

    refactoring phase

--- src/__test__/main-page.test.js
+++ src/__test__/main-page.test.js
@@ -5,6 +5,6 @@ import { MainPage } from '../components/main-page';
 describe('main page mount', () => {
     it('must display the main page title', () => {
         render(<MainPage />)
-        expect(screen.getByText(/simpsons quotes/i)).toBeInTheDocument();
+        expect(screen.getByRole('heading', { name: /simpsons quotes/i })).toBeInTheDocument();
     })
 })
