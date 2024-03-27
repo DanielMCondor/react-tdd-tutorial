4b9980a3dfc0cb31babc92a31985e8ea614e602a
DanielMCondor <dmantillacondor@gmail.com>
2024-03-26 19:09:29 -0500

    refactoring test code smell

--- src/__test__/main-page.test.js
+++ src/__test__/main-page.test.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MainPage } from '../components/main-page';
 
+beforeEach(() => render(<MainPage />));
+
 describe('main page mount', () => {
     it('must display the main page title', () => {
-        render(<MainPage />)
         expect(screen.getByRole('heading', { name: /simpsons quotes/i })).toBeInTheDocument();
     })
 })
 
 describe('Quotes List', () => {
     it('must display 3 quotes', async () => {
-        render(<MainPage />)
         expect(await screen.findAllByRole('listitem')).toHaveLength(3);
     })
 })
