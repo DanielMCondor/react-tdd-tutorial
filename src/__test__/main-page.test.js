15732fe9e11732eda4ab2001d79e74f18ed455d6
DanielMCondor <dmantillacondor@gmail.com>
2024-03-26 19:15:55 -0500

    must contain quote value

--- src/__test__/main-page.test.js
+++ src/__test__/main-page.test.js
@@ -14,4 +14,10 @@ describe('Quotes List', () => {
     it('must display 3 quotes', async () => {
         expect(await screen.findAllByRole('listitem')).toHaveLength(3);
     })
+
+    it('must contain quote value', async () => {
+        const [firstQuote, secondQuote, thirdQuote] = await screen.findAllByRole('listitem');
+        expect(firstQuote.textContent).toBe('Gah, stupid sexy Flanders!');
+        expect(secondQuote.textContent).toBe('Eat my shorts');
+    })
 })
